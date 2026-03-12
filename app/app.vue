7cab50e13ad0017df9e86b9e6125f5066116ca56
lane.pang <lane.pang@jingdigital.com>
2026-03-12 21:11:33 +0800

    feat: monitor

--- app/app.vue
+++ app/app.vue
@@ -36,6 +36,15 @@ useHead({
         gtag('js', new Date());
         gtag('config', 'G-DNG9SW4Q5T');
       `
+    },
+    {
+      innerHTML: `
+        var ahrefs_analytics_script = document.createElement('script');
+  ahrefs_analytics_script.async = true;
+  ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
+  ahrefs_analytics_script.setAttribute('data-key', 'Lz+NingVl66hJpI31hJTXQ');
+  document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
+      `
     }
   ]
 })
